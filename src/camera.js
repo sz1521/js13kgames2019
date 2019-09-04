b0f0fb0653ee6bacf05283fd6fdabb291adec09d
Tero Jäntti <tkjantti@gmail.com>
2019-09-04 21:44:29 +0300

    Camera refactoring.

--- src/camera.js
+++ src/camera.js
@@ -48,42 +48,47 @@ export const createCamera = (level, canvas) => {
 
     update() {
       if (this.target) {
-        let newX, newY;
-
-        newX = this.target.x + this.target.width;
-        newY = this.target.y + this.target.height;
-
-        const zoomedWidth = level.width * this.zoom;
-        const zoomedHeight = level.height * this.zoom;
-
-        // Zoom such that camera stays within the level.
-        if (zoomedWidth < canvas.width || zoomedHeight < canvas.height) {
-          this.zoom = Math.max(
-            canvas.width / level.width,
-            canvas.height / level.height
-          );
-        }
-
-        const viewAreaWidth = canvas.width / this.zoom;
-        const viewAreaHeight = canvas.height / this.zoom;
-
-        // Keep camera within level in x-direction.
-        if (newX - viewAreaWidth / 2 < level.left) {
-          newX = level.left + viewAreaWidth / 2;
-        } else if (newX + viewAreaWidth / 2 > level.width) {
-          newX = level.width - viewAreaWidth / 2;
-        }
-
-        // Keep camera within level in y-direction.
-        if (newY - viewAreaHeight / 2 < level.top) {
-          newY = level.top + viewAreaHeight / 2;
-        } else if (newY + viewAreaHeight / 2 > level.height) {
-          newY = level.height - viewAreaHeight / 2;
-        }
-
-        this.x = newX;
-        this.y = newY;
+        this._fitZoom();
+        this._follow();
       }
+    },
+
+    _fitZoom() {
+      const zoomedWidth = level.width * this.zoom;
+      const zoomedHeight = level.height * this.zoom;
+
+      // Zoom such that camera stays within the level.
+      if (zoomedWidth < canvas.width || zoomedHeight < canvas.height) {
+        this.zoom = Math.max(
+          canvas.width / level.width,
+          canvas.height / level.height
+        );
+      }
+    },
+
+    _follow() {
+      let x = this.target.x + this.target.width;
+      let y = this.target.y + this.target.height;
+
+      const viewAreaWidth = canvas.width / this.zoom;
+      const viewAreaHeight = canvas.height / this.zoom;
+
+      // Keep camera within level in x-direction.
+      if (x - viewAreaWidth / 2 < level.left) {
+        x = level.left + viewAreaWidth / 2;
+      } else if (x + viewAreaWidth / 2 > level.width) {
+        x = level.width - viewAreaWidth / 2;
+      }
+
+      // Keep camera within level in y-direction.
+      if (y - viewAreaHeight / 2 < level.top) {
+        y = level.top + viewAreaHeight / 2;
+      } else if (y + viewAreaHeight / 2 > level.height) {
+        y = level.height - viewAreaHeight / 2;
+      }
+
+      this.x = x;
+      this.y = y;
     }
   };
 };
